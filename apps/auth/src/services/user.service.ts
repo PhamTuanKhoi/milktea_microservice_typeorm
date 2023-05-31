680c64c2551133041f772fe500cb26e5b72197d4
huynhanhpham <huynhanhpham734@gmail.com>
2023-05-31 10:29:00 +0700

    import logger

--- apps/auth/src/services/user.service.ts
+++ apps/auth/src/services/user.service.ts
@@ -1,5 +1,11 @@
 import { UserEntity } from '@app/common';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RegisterRequest } from 'libs/gobal/src';
 import { Repository } from 'typeorm';
@@ -7,6 +13,8 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
+
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
@@ -37,20 +45,26 @@ export class UserService {
   async register(registerRequest: RegisterRequest) {
     let { password, email } = registerRequest;
 
-    const user = await this.findByEmail(email);
+    try {
+      const user = await this.findByEmail(email);
 
-    if (user)
-      throw new HttpException('email already exists!', HttpStatus.CONFLICT);
+      if (user)
+        throw new HttpException('email already exists!', HttpStatus.CONFLICT);
 
-    password = await this.hashPassword(password);
+      password = await this.hashPassword(password);
 
-    const created = await this.userRepository.save({
-      ...registerRequest,
-      password,
-    });
+      const created = await this.userRepository.save({
+        ...registerRequest,
+        password,
+      });
 
-    delete created.password;
+      delete created.password;
 
-    return created;
+      this.logger.log(`register a new user by id#${created?.id}`);
+      return created;
+    } catch (error) {
+      this.logger.error(error.message, error.stack);
+      throw new BadRequestException();
+    }
   }
 }
